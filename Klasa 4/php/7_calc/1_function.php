ba995cd061c390d6f602062585c57da97b31df7b
Mikołaj Napierała <miki.n.2001@gmail.com>
2020-11-09 12:14:34 +0100

    php: 7

---
+++ Klasa 4/php/7_calc/1_function.php
@@ -0,0 +1,16 @@
+<?php
+    function add($x, $y){
+        return $x + $y;
+    }
+
+    function subt($x, $y){
+        return $x - $y;
+    }
+
+    function multi($x, $y){
+        return $x * $y;
+    }
+
+    function divi($x, $y){
+        return $x / $y;
+    }
